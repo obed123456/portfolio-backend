237d201588330208d52cf7d0f5fe90c78755861e
obed123456 <obedsam0022013@gmail.com>
2018-05-10 23:43:06 +0200

    make it better

---
+++ rest.js
@@ -0,0 +1,46 @@
+var mysql = require('mysql');
+
+function REST_ROUTER(router,connection,md5) {
+  var self = this;
+  self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+  router.get("/",function(req,res){
+      res.json({"Message" : "Connected with api!"});
+  });
+
+//All endpoints goes here
+
+//get all comments
+router.get("/getrating",function(req,res){
+    var query = "SELECT * FROM ??";
+    var table = ["rating"];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if(err) {
+            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+        }
+    });
+  });
+
+//POST request
+router.post("/postrating",function(req,res){
+    var query = "INSERT INTO ??(??) VALUES (?)";
+    var table = ["rating","rating",req.body.rating];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if(err) {
+            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.json({"Error" : false, "Message" : "Rating submitted!"});
+        }
+    });
+});
+
+
+
+}
+module.exports = REST_ROUTER;
